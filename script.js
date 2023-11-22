5b4868cd4e6c01ff837c03dc6b83d22e02f5d575
ukrstam <151749884+ukrstam@users.noreply.github.com>
2023-11-22 15:24:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+document.getElementById('invitationForm').addEventListener('submit', function() {
+    document.getElementById('message').textContent = "Hmm, try to get your invitation first.";
+});
 
